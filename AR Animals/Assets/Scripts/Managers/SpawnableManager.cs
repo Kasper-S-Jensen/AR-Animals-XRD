f1527960bb1e7d4a472c1435ac3e6321fa35d859
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-09-28 11:47:28 +0200

    Add touch input registration (Kasper, Kutaiba)

---
+++ AR Animals/Assets/Scripts/Managers/SpawnableManager.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class SpawnableManager : MonoBehaviour
+{
+    public GameEvent onAnimalClick;
+    
+    [SerializeField] private ARRaycastManager _arRaycastManager;
+    private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
+
+    [SerializeField] private GameObject spawnablePrefab;
+
+    private Camera arCam;
+    private GameObject spawnedObject;
+
+    private void Start()
+    {
+        spawnedObject = null;
+        arCam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount == 0)
+        {
+            return;
+        }
+
+        if (_arRaycastManager.Raycast(Input.GetTouch(0).position, _hits))
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began && spawnedObject == null)
+            {
+                Ray ray = arCam.ScreenPointToRay(Input.GetTouch(0).position);
+                if (Physics.Raycast(ray, out var hit))
+                {
+                    if (hit.collider.gameObject.CompareTag("Animal"))
+                    {
+                        spawnedObject = hit.collider.gameObject;
+                        onAnimalClick.Raise();
+                    }
+                    else
+                    {
+                     //   SpawnPrefab(_hits[0].pose.position);
+                    }
+                }
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Moved && spawnedObject != null)
+            {
+                spawnedObject.transform.position = _hits[0].pose.position;
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                spawnedObject = null;
+            }
+        }
+    }
+
+    private void SpawnPrefab(Vector3 spawnPosition)
+    {
+        spawnedObject = Instantiate(spawnablePrefab, spawnPosition, Quaternion.identity);
+    }
+}
