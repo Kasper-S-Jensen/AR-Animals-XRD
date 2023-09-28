bd6358f51bbf74e05a4991b79ed776687fdf7dce
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-09-28 09:34:11 +0200

    Add Canvas and event system (Kasper, Kutaiba))

---
+++ AR Animals/Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameEvent")]
+public class GameEvent : ScriptableObject
+{
+    public List<GameEventListener> listeners = new();
+
+
+    //overloading raise to allow for sending data and information of sender. This is optional
+    public void Raise()
+    {
+        Raise(null, null);
+    }
+
+    public void Raise(object data)
+    {
+        Raise(null, data);
+    }
+
+    public void Raise(Component sender)
+    {
+        Raise(sender, null);
+    }
+
+    public void Raise(Component sender, object data)
+    {
+        for (var i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised(sender, data);
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        if (!listeners.Contains(listener))
+        {
+            listeners.Add(listener);
+        }
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        if (listeners.Contains(listener))
+        {
+            listeners.Remove(listener);
+        }
+    }
+}
---
+++ AR Animals/Assets/Scripts/Events/GameEventListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class CustomGameEvent : UnityEvent<Component, object>
+{
+}
+
+public class GameEventListener : MonoBehaviour
+{
+    [Tooltip("Event to listen to")] public GameEvent gameEvent;
+
+    [Tooltip("Response to invoke when Event is raised.")]
+    public CustomGameEvent response;
+
+    private void OnEnable()
+    {
+        gameEvent.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        gameEvent.UnregisterListener(this);
+    }
+
+    public void OnEventRaised(Component sender, object data)
+    {
+        response.Invoke(sender, data);
+    }
+}
